a2c8281409201b7df115a99f3ad7c1a2a3e3b653
renuka0912 <23215a6708@bvrit.ac.in>
2024-09-10 17:18:35 +0530

    docker into my git repo

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          docker
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>hello world</h1>
+        <h1>docker</h1>
+          docker
+        <h1>this is renuka</h1>
       </header>
     </div>
   );
